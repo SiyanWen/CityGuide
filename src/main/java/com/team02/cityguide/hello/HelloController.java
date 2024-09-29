620f96456acd9e0f580861bda16f376ddcaee926
Eva Zhang <evaza@uw.edu>
2024-09-28 17:20:02 -0700

    Hello

--- src/main/java/com/team02/cityguide/hello/HelloController.java
+++ src/main/java/com/team02/cityguide/hello/HelloController.java
@@ -1,12 +1,24 @@
 package com.team02.cityguide.hello;
 
+import net.datafaker.Faker;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
     @GetMapping("/hello")
-    public String hello() {
-        return "Hello World!";
+    public Person sayHello(@RequestParam(required = false) String name) {
+        if (name == null) {
+            name = "Guest";
+        }
+        Faker faker = new Faker();
+        String company = faker.company().name();
+        String street = faker.address().streetAddress();
+        String city = faker.address().city();
+        String state = faker.address().state();
+        String bookTitle = faker.book().title();
+        String bookAuthor = faker.book().author();
+        return new Person(name, company, new Address(street, city, state, null), new Book(bookTitle, bookAuthor));
     }
 }
