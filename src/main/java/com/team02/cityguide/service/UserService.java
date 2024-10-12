180c082c7a62c909f3888050444cc88c612bd93a
zmtlandl <61840721+zmtlandl@users.noreply.github.com>
2024-10-12 19:28:10 -0400

    Update UserService.java

--- src/main/java/com/team02/cityguide/service/UserService.java
+++ src/main/java/com/team02/cityguide/service/UserService.java
@@ -4,17 +4,55 @@ import com.team02.cityguide.entity.UserEntity;
 import com.team02.cityguide.repository.UserRepository;
 import org.springframework.stereotype.Service;
 
+import com.team02.cityguide.entity.RouteEntity;
+import com.team02.cityguide.entity.RouteGalleryEntity;
+import com.team02.cityguide.entity.SpotGalleryEntity;
+import com.team02.cityguide.entity.UnitRouteEntity;
+import com.team02.cityguide.entity.UerEntity;
+import com.team02.cityguide.entity.UserSpotEntity;
+
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.UserDetailsManager;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
 @Service
 public class UserService {
     private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final UserDetailsManager userDetailsManager;
 
-    public UserService(UserRepository userRepository) {
+    public UserService(
+        UserRepository userRepository
+        PasswordEncoder passwordEncoder,
+        UserDetailsManager userDetailsManager) {
         this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+        this.userDetailsManager = userDetailsManager;
     }
 
     public UserEntity findByEmail(String email) {
         return userRepository.findByEmail(email);
     }
 
-    // TODO
+    @Transactional
+    public void signUp(String email, String password, String firstName, String lastName) {
+        email = email.toLowerCase();
+        UserDetails user = User.builder()
+                .username(email)
+                .password(passwordEncoder.encode(password))
+                .roles("USER")
+                .build();
+        userDetailsManager.createUser(user);
+        customerRepository.updateNameByEmail(email);
+
+
+        CustomerEntity savedCustomer = customerRepository.findByEmail(email);
+        // CartEntity cart = new CartEntity(null, savedCustomer.id(), 0.0);
+        // cartRepository.save(cart);
+    }
+
+   
 }
