a16f1063056bd06727ccab802bd1370f43703cb6
zmtlandl <61840721+zmtlandl@users.noreply.github.com>
2024-10-12 18:56:06 -0400

    Update UserRepository.java

--- src/main/java/com/team02/cityguide/repository/UserRepository.java
+++ src/main/java/com/team02/cityguide/repository/UserRepository.java
@@ -6,7 +6,10 @@ import org.springframework.data.jdbc.repository.query.Query;
 import org.springframework.data.repository.ListCrudRepository;
 
 public interface UserRepository extends ListCrudRepository<UserEntity, Long> {
+    // List<UserEntity> findByFirstName(String firstName);
 
+    // List<UserEntity> findByLastName(String lastName);
+    
     UserEntity findByEmail(String email);
 
     void deleteByEmail(String email);
