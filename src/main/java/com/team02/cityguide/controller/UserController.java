433b9bb780358a7215e8a3855065bd798f79d7b4
zmtlandl <61840721+zmtlandl@users.noreply.github.com>
2024-10-12 20:56:33 -0400

    Update UserController.java

--- src/main/java/com/team02/cityguide/controller/UserController.java
+++ src/main/java/com/team02/cityguide/controller/UserController.java
@@ -14,10 +14,10 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserController {
 
 
- private final UserService UserService;
+ private final UserService userService;
 
 
- public UserController(CustomerService userService) {
+ public UserController(UserService userService) {
  this.userService = userService;
  }
 
