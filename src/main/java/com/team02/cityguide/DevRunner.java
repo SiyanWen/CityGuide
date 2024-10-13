84712540c6182b1d3e6657e5daafdfff00ea2b2e
zmtlandl <61840721+zmtlandl@users.noreply.github.com>
2024-10-12 20:26:22 -0400

    Update DevRunner.java

--- src/main/java/com/team02/cityguide/DevRunner.java
+++ src/main/java/com/team02/cityguide/DevRunner.java
@@ -4,12 +4,27 @@ import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.stereotype.Component;
 
+import com.laioffer.onlineorder.service.CustomerService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
 @Component
 public class DevRunner implements ApplicationRunner {
-    // TODO
+    private static final Logger logger = LoggerFactory.getLogger(DevRunner.class);
+
+
+    private final UserService userService;
+
+
+    public DevRunner(
+           UserService userService) {
+       this.userService = userService;
+    }
+
 
     @Override
     public void run(ApplicationArguments args) throws Exception {
-        // TODO
+        customerService.signUp("foo@mail.com", "123456", "Foo", "Bar");
     }
 }
